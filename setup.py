554ad3246ae7643495ac5cbe9184ffa2b73b6d24
Prabasandev <33477861+Prabasandev@users.noreply.github.com>
2018-10-09 19:24:05 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ import re, ast
 
 # get version from __version__ variable in candidate_registration/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 with open('candidate_registration/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='candidate_registration',
@@ -21,6 +23,7 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
+	#install_requires=[str(ir.req) for ir in requirements],
+	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
